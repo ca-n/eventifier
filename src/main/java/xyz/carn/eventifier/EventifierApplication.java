1d29b07c0652be57cd1b601fca1c21af3c94f62a
Neil Carrie <neil.carrie@gmail.com>
2022-03-01 22:51:12 -0500

    Create initial project

---
+++ src/main/java/xyz/carn/eventifier/EventifierApplication.java
@@ -0,0 +1,13 @@
+package xyz.carn.eventifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/carn/eventifier/EventifierApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.carn.eventifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
