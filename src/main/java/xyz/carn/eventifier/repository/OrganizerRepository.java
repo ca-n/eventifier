435e2dee91e28b1739e0d437fab9730028084bae
Neil Carrie <neil.carrie@gmail.com>
2022-03-10 23:07:19 -0500

    Add login function to OrganizerRepository

--- src/main/java/xyz/carn/eventifier/repository/OrganizerRepository.java
+++ src/main/java/xyz/carn/eventifier/repository/OrganizerRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import xyz.carn.eventifier.model.Organizer;
 
+import java.util.Optional;
+
 @Repository
 public interface OrganizerRepository extends JpaRepository<Organizer, Long> {
+    Optional<Organizer> findByEmailAndPassword(String email, String password);
 }
