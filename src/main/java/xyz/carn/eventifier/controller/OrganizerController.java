c3803fe7398badc3c45aca30b8a749c74fd0db97
Neil Carrie <neil.carrie@gmail.com>
2022-03-11 12:15:22 -0500

    Add login to OrganizerController

--- src/main/java/xyz/carn/eventifier/controller/OrganizerController.java
+++ src/main/java/xyz/carn/eventifier/controller/OrganizerController.java
@@ -1,8 +1,10 @@
 package xyz.carn.eventifier.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import xyz.carn.eventifier.model.Organizer;
+import xyz.carn.eventifier.model.User;
 import xyz.carn.eventifier.service.OrganizerService;
 
 @RestController
@@ -18,5 +20,12 @@ public class OrganizerController {
     @PostMapping
     public ResponseEntity<Organizer> register(@RequestBody Organizer organizer) {
         return ResponseEntity.ok(service.register(organizer));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Organizer> login(@RequestBody User credentials) {
+        return service.login(credentials)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
 }
