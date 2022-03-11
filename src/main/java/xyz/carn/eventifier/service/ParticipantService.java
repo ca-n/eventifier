525703163802f3dcf21d00a155ca0dda8662fe80
Neil Carrie <neil.carrie@gmail.com>
2022-03-11 12:15:39 -0500

    Add login to ParticipantService

--- src/main/java/xyz/carn/eventifier/service/ParticipantService.java
+++ src/main/java/xyz/carn/eventifier/service/ParticipantService.java
@@ -2,7 +2,10 @@ package xyz.carn.eventifier.service;
 
 import org.springframework.stereotype.Service;
 import xyz.carn.eventifier.model.Participant;
+import xyz.carn.eventifier.model.User;
 import xyz.carn.eventifier.repository.ParticipantRepository;
+
+import java.util.Optional;
 
 @Service
 public class ParticipantService {
@@ -14,5 +17,9 @@ public class ParticipantService {
 
     public Participant register(Participant participant) {
         return repository.save(participant);
+    }
+
+    public Optional<Participant> login(User credentials) {
+        return repository.findByEmailAndPassword(credentials.getEmail(), credentials.getPassword());
     }
 }
