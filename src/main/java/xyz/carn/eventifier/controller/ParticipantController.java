d160ad31acda70544891d84faf65f2aa46ad7f16
Neil Carrie <neil.carrie@gmail.com>
2022-03-11 12:15:53 -0500

    Add login to ParticipantController

--- src/main/java/xyz/carn/eventifier/controller/ParticipantController.java
+++ src/main/java/xyz/carn/eventifier/controller/ParticipantController.java
@@ -1,8 +1,10 @@
 package xyz.carn.eventifier.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import xyz.carn.eventifier.model.Participant;
+import xyz.carn.eventifier.model.User;
 import xyz.carn.eventifier.service.ParticipantService;
 
 @RestController
@@ -18,5 +20,12 @@ public class ParticipantController {
     @PostMapping
     public ResponseEntity<Participant> register(@RequestBody Participant participant) {
         return ResponseEntity.ok(service.register(participant));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Participant> login(@RequestBody User credentials) {
+        return service.login(credentials)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
 }
