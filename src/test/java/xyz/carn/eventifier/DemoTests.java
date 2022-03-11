e4c9eba549a894a88cacd367e14fc132b643de4a
Neil Carrie <neil.carrie@gmail.com>
2022-03-11 13:12:09 -0500

    Create DemoTests

---
+++ src/test/java/xyz/carn/eventifier/DemoTests.java
@@ -0,0 +1,19 @@
+package xyz.carn.eventifier;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class DemoTests {
+
+    @Test
+    public void thisTestWillPass() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void thisTestWillFail() {
+        fail();
+    }
+}
